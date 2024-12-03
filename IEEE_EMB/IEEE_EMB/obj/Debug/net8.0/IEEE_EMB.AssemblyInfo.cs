794cbbee710bdcc93dd793b2fbd609275e66e9d0
Amira-Sherif1 <s-amira.nassef@zewailcity.edu.eg>
2024-12-03 23:30:36 +0200

    forms

--- IEEE_EMB/IEEE_EMB/obj/Debug/net8.0/IEEE_EMB.AssemblyInfo.cs
+++ IEEE_EMB/IEEE_EMB/obj/Debug/net8.0/IEEE_EMB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IEEE_EMB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0309cd18e3ac74b7e50eaf16b84fd1de605db57c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d7357634726a653538967ce2e92582591590c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("IEEE_EMB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IEEE_EMB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
