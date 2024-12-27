cad748e7bd210016d22125340df83b1977515899
Amira-Sherif1 <s-amira.nassef@zewailcity.edu.eg>
2024-12-27 22:36:57 +0200

    statistics

---
+++ IEEE_EMB/IEEE_EMB/Pages/Admin/errorpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IEEE_EMB.Pages.Admin
+{
+    public class error_page_cshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
