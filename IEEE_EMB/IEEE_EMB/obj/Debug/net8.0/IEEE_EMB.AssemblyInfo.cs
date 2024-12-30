5729a81a003b21cbc493f1f628f8e3ad26c0c3e3
EngOayoubFo22 <omar.ayoub005@gmail.com>
2024-12-30 21:50:46 +0200

    .

--- IEEE_EMB/IEEE_EMB/obj/Debug/net8.0/IEEE_EMB.AssemblyInfo.cs
+++ IEEE_EMB/IEEE_EMB/obj/Debug/net8.0/IEEE_EMB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IEEE_EMB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f390058d4dbe0232cc743a049297a4302ead5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06108097f3f7f5387b8a46e44cc19b4bff0353b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("IEEE_EMB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IEEE_EMB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
